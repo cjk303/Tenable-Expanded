f4b4d59c2dae6295db57fbc649f539806ac5efdb
cjk303 <kj1@poczta.fm>
2025-09-18 12:22:27 +0200

    Update app.py

--- tenpre/app.py
+++ tenpre/app.py
@@ -6,13 +6,6 @@
     force_relink: true
     ansible_remote_tmp: /tmp
 
-  pre_tasks:
-    - name: Gather minimal OS facts
-      setup:
-        gather_subset:
-          - '!all'
-          - 'min'
-
   tasks:
 
     # ---------------- Remove Rapid7 ----------------
@@ -22,17 +15,15 @@
         dest: /tmp/r7remover.sh
         mode: "0755"
 
-    - name: Run Rapid7 remover (dzdo password passed manually)
+    - name: Run Rapid7 remover with dzdo
       shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S /bin/bash /tmp/r7remover.sh"
       become: no
       when: remove_rapid7 | bool
 
     # ---------------- Remove existing Nessus token ----------------
     - name: Remove existing Nessus token if force_relink
-      shell: /opt/nessus_agent/sbin/nessuscli agent unlink
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S /opt/nessus_agent/sbin/nessuscli agent unlink"
+      become: no
       ignore_errors: yes
       when: force_relink
 
@@ -47,10 +38,8 @@
         - ansible_facts['distribution_major_version'] == "7"
 
     - name: Install RHEL 7 package
-      shell: yum -y localinstall /tmp/el7.rpm
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S yum -y localinstall /tmp/el7.rpm"
+      become: no
       when:
         - ansible_facts['distribution'] == "RedHat"
         - ansible_facts['distribution_major_version'] == "7"
@@ -65,10 +54,8 @@
         - ansible_facts['distribution_major_version'] == "8"
 
     - name: Install RHEL 8 package
-      shell: dnf -y localinstall /tmp/el8.rpm
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S dnf -y localinstall /tmp/el8.rpm"
+      become: no
       when:
         - ansible_facts['distribution'] == "RedHat"
         - ansible_facts['distribution_major_version'] == "8"
@@ -83,10 +70,8 @@
         - ansible_facts['distribution_major_version'] == "9"
 
     - name: Install RHEL 9 package
-      shell: dnf -y localinstall /tmp/el9.rpm
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S dnf -y localinstall /tmp/el9.rpm"
+      become: no
       when:
         - ansible_facts['distribution'] == "RedHat"
         - ansible_facts['distribution_major_version'] == "9"
@@ -100,29 +85,21 @@
       when: ansible_facts['distribution'] in ['Ubuntu', 'Debian']
 
     - name: Install Debian/Ubuntu package
-      shell: apt-get install -y /tmp/NessusAgent-10.9.0-ubuntu1604_amd64.deb
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S apt-get install -y /tmp/NessusAgent-10.9.0-ubuntu1604_amd64.deb"
+      become: no
       when: ansible_facts['distribution'] in ['Ubuntu', 'Debian']
 
     # ---------------- Start & Enable Nessus Agent ----------------
     - name: Enable and start Nessus Agent
-      systemd:
-        name: nessusagent
-        state: started
-        enabled: yes
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      shell: "echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S systemctl enable --now nessusagent"
+      become: no
 
     - name: Link Nessus Agent
       shell: >
+        echo '{{ ansible_become_password }}' | {{ hostvars[inventory_hostname]['ansible_become_method'] }} -S
         /opt/nessus_agent/sbin/nessuscli agent link
         --key={{ activation_key }}
         {% if mode == "cloud" %} --cloud {% else %} --manager-host={{ manager_host }} --manager-port={{ manager_port }} {% endif %}
       args:
         creates: "/opt/nessus_agent/.nessus/agent.key"
-      become: yes
-      become_method: "{{ hostvars[inventory_hostname]['ansible_become_method'] }}"
-      become_flags: "-S -tt"
+      become: no
